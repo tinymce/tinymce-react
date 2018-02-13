391395713e2c3c68db3526dc5da1973ef45f22bc
Mattias Wikstrom <mattias.wikstrom@ephox.com>
2018-02-13 17:05:38 +0100

    lint fakeGlobal

--- test/fakeGlobal.ts
+++ test/fakeGlobal.ts
@@ -1,4 +1,8 @@
-(global as any).tinymce = {init: (x: any) => { return; }};
+(global as any).tinymce = {
+  init: (x: any) => {
+    return;
+  }
+};
 (global as any).requestAnimationFrame = (callback: any) => {
   setTimeout(callback, 0);
 };
