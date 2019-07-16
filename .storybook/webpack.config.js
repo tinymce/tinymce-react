539405f599ae512d6254fa105a6b68a90507afe5
Simon Fjeldså <simon.fjeldsa@tiny.cloud>
2019-07-16 10:53:01 +0200

    Upgrade @Storybook, fix braking change

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        include: [
-          path.join(__dirname, '../src'),
-          path.join(__dirname, '../stories')
-        ],
-        options: {
-          transpileOnly: true
-        }
-      }
-    ]
-  },
-  resolve: {
+module.exports = async ({ config, mode }) => {
+
+  config.module.rules.push({
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    include: [
+      path.join(__dirname, '../src'),
+      path.join(__dirname, '../stories')
+    ],
+    options: {
+      transpileOnly: true
+    }
+  });
+
+  config.resolve =  {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
-  }
+  };
+
+  return config;
 };
