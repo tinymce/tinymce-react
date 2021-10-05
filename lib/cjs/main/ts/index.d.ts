dcf2c0d7b30148684c720d7ed77b3a17847b41ee
alextkd <lazar.alexandru.emil@gmail.com>
2021-10-05 13:45:03 +0300

    PIA: revert lib change

--- lib/cjs/main/ts/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2017-present, Ephox, Inc.
- *
- * This source code is licensed under the Apache 2 license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-import { Editor, IAllProps } from './components/Editor';
-export { Editor, IAllProps };
