8f8ffa1138c07623afd7af948e205d37b01e0981
Daniel Oakman <141111365+danoaky-tiny@users.noreply.github.com>
2024-03-22 09:04:12 +1100

    INT-3291: Remove `CloudHostedProps` and `HybridOrSelfHostedProps` from `IEditorPropTypes`

--- src/main/ts/components/EditorPropTypes.ts
+++ src/main/ts/components/EditorPropTypes.ts
@@ -1,12 +1,12 @@
 import * as PropTypes from 'prop-types';
 import { IEvents } from '../Events';
-import { HybridOrSelfHostedProps, IProps, CloudHostedProps } from './Editor';
+import { IProps } from './Editor';
 
 export type CopyProps<T> = { [P in keyof T]: PropTypes.Requireable<unknown> };
 
 export type IEventPropTypes = CopyProps<IEvents>;
 
-export interface IEditorPropTypes extends IEventPropTypes, CopyProps<IProps & CloudHostedProps & HybridOrSelfHostedProps> {}
+export interface IEditorPropTypes extends IEventPropTypes, CopyProps<IProps> {}
 
 export const eventPropTypes: IEventPropTypes = {
   onActivate: PropTypes.func,
