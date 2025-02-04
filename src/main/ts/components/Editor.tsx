a62e408c96266e0792a8ead1cba875bf051fcd9e
Shubhamservaapty <Shubham.serva@apty.io>
2025-02-04 17:44:28 +0530

    removed remote URL

--- src/main/ts/components/Editor.tsx
+++ src/main/ts/components/Editor.tsx
@@ -309,11 +309,6 @@ export class Editor extends React.Component<IAllProps> {
         }
       });
     }
-    // fallback to the cloud when the tinymceScriptSrc is not specified
-    const channel = this.props.cloudChannel as Version; // `cloudChannel` is in `defaultProps`, so it's always defined.
-    const apiKey = this.props.apiKey ? this.props.apiKey : 'no-api-key';
-    const cloudTinyJs = `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${channel}/tinymce.min.js`;
-    return [{ src: cloudTinyJs, async, defer }];
   }
 
   private getInitialValue() {
--- src/test/ts/browser/LoadTinyTest.ts
+++ src/test/ts/browser/LoadTinyTest.ts
@@ -39,38 +39,4 @@ describe('LoadTinyTest', () => {
       assertTinymceVersion(version);
     });
   });
-
-  CLOUD_VERSIONS.forEach((version) => {
-    it(`Should be able to load TinyMCE from Cloud (${version})`, async () => {
-      const apiKey = 'a-fake-api-key';
-      using _ = await render({ apiKey, cloudChannel: version });
-      assertTinymceVersion(version);
-      Assertions.assertEq(
-        'TinyMCE should have been loaded from Cloud',
-        `https://cdn.tiny.cloud/1/${apiKey}/tinymce/${version}`,
-        Global.tinymce.baseURI.source
-      );
-    });
-
-    it(`Should be able to load TinyMCE (${version}) in hybrid`, async () => {
-      using _ = await render({
-        tinymceScriptSrc: [
-          `/project/node_modules/tinymce-${version}/tinymce.min.js`,
-          `https://cdn.tiny.cloud/1/${VALID_API_KEY}/tinymce/${version}/cloud-plugins.min.js?tinydrive=${version}`
-        ],
-        plugins: [ 'tinydrive' ]
-      });
-      assertTinymceVersion(version);
-      Assertions.assertEq(
-        'TinyMCE should have been loaded locally',
-        `/project/node_modules/tinymce-${version}`,
-        Global.tinymce.baseURI.path
-      );
-      Assertions.assertEq(
-        'The tinydrive plugin should have defaults for the cloud',
-        `https://cdn.tiny.cloud/1/${VALID_API_KEY}/tinymce-plugins/tinydrive/${version}/plugin.min.js`,
-        (Global.tinymce.defaultOptions || Global.tinymce.defaultSettings)?.custom_plugin_urls?.tinydrive
-      );
-    });
-  });
 });
