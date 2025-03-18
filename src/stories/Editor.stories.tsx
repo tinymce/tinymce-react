61241face03de4904f297604f486ea125d076d27
Hamza Benkhaldoun <hamza0867@gmail.com>
2025-03-18 06:22:00 +0000

    INT-3226: Remove console log

--- src/stories/Editor.stories.tsx
+++ src/stories/Editor.stories.tsx
@@ -61,7 +61,6 @@ export const ControlledInput: StoryObj<Editor> = {
           apiKey={apiKey}
           value={data}
           onEditorChange={(e) => {
-            console.log('change');
             setData(e);
           }}
         />
