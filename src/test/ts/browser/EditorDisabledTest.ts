93815f48c4a55d5db6b1df2df1bdced0f4d86395
Ben Tran <ben.tran@tiny.cloud>
2025-05-09 15:16:43 +0930

    TINY-11906: Update tests

--- src/test/ts/browser/EditorDisabledTest.ts
+++ src/test/ts/browser/EditorDisabledTest.ts
@@ -4,78 +4,74 @@ import { Assertions, Waiter } from '@ephox/agar';
 
 describe('EditorDisabledTest', () => {
 
-  context('with TinyMCE before 7.6', () => {
-    ([ '7.5' ] as Array<any>).forEach((version) =>
-      Loader.withVersion(version, (render) => {
-        it('updating disabled prop should toggle the editor\'s mode', async () => {
-          using ctx = await render({
-            disabled: true
-          });
-
-          Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
-
-          await ctx.reRender({
-            disabled: false
-          });
-          await Waiter.pTryUntil('mode is changed to design', () => {
-            Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
-          });
+  context('with TinyMCE < 7.6', () => {
+    Loader.withVersion('7.5', (render) => {
+      it('updating disabled prop should toggle the editor\'s mode', async () => {
+        using ctx = await render({
+          disabled: true
         });
 
-        it('updating readonly prop should toggle the editor\'s mode', async () => {
-          using ctx = await render({
-            readonly: true
-          });
-          Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
+        Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
 
-          await ctx.reRender({
-            readonly: false
-          });
-          await Waiter.pTryUntil('mode is changed to design', () => {
-            Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
-          });
+        await ctx.reRender({
+          disabled: false
         });
-      })
-    );
+        await Waiter.pTryUntil('mode is changed to design', () => {
+          Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
+        });
+      });
+
+      it('updating readonly prop should toggle the editor\'s mode', async () => {
+        using ctx = await render({
+          readonly: true
+        });
+        Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
+
+        await ctx.reRender({
+          readonly: false
+        });
+        await Waiter.pTryUntil('mode is changed to design', () => {
+          Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
+        });
+      });
+    });
   });
 
-  context('with TinyMCE 7.6 and above', () => {
-    ([ '7' ] as Array<any>).forEach((version) =>
-      Loader.withVersion(version, (render) => {
-        it('updating disabled prop should only change the editor\'s state', async () => {
-          using ctx = await render({
-            disabled: true
-          });
+  context('with TinyMCE >= 7.6', () => {
+    Loader.withVersion('7', (render) => {
+      it('updating disabled prop should only change the editor\'s state', async () => {
+        using ctx = await render({
+          disabled: true
+        });
 
+        Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
+        Assertions.assertEq('editor is disabled', true, ctx.editor.options.get('disabled'));
+
+        await ctx.reRender({
+          disabled: false
+        });
+
+        await Waiter.pTryUntil('editor\'s state should be updated', () => {
           Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
-          Assertions.assertEq('editor is disabled', true, ctx.editor.options.get('disabled'));
-
-          await ctx.reRender({
-            disabled: false
-          });
-
-          await Waiter.pTryUntil('editor\'s state should be updated', () => {
-            Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
-            Assertions.assertEq('editor is not disabled', false, ctx.editor.options.get('disabled'));
-          });
-        });
-
-        it('updating readonly prop should only change the editor\'s mode', async () => {
-          using ctx = await render({
-            readonly: true
-          });
-          Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
           Assertions.assertEq('editor is not disabled', false, ctx.editor.options.get('disabled'));
-
-          await ctx.reRender({
-            readonly: false
-          });
-          await Waiter.pTryUntil('editor\'s mode should be updated', () => {
-            Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
-            Assertions.assertEq('editor is not disabled', false, ctx.editor.options.get('disabled'));
-          });
         });
-      })
-    );
+      });
+
+      it('updating readonly prop should only change the editor\'s mode', async () => {
+        using ctx = await render({
+          readonly: true
+        });
+        Assertions.assertEq('mode is readonly', 'readonly', ctx.editor.mode.get());
+        Assertions.assertEq('editor is not disabled', false, ctx.editor.options.get('disabled'));
+
+        await ctx.reRender({
+          readonly: false
+        });
+        await Waiter.pTryUntil('editor\'s mode should be updated', () => {
+          Assertions.assertEq('mode is design', 'design', ctx.editor.mode.get());
+          Assertions.assertEq('editor is not disabled', false, ctx.editor.options.get('disabled'));
+        });
+      });
+    });
   });
 });
